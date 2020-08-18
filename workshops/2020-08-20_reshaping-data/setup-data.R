8e0ea186b9d9ce81b452cb4b5e66575b46d909f2
Victor <victor.2wy@gmail.com>
2020-08-18 11:26:06 -0700

    transpose data

--- workshops/2020-08-20_reshaping-data/setup-data.R
+++ workshops/2020-08-20_reshaping-data/setup-data.R
@@ -5,9 +5,28 @@ data
 
 pdat <- data %>% select(expr_geo_id:ga_days)
 
-expr <- data %>% select(expr_geo_id, contains('transcript'))
 
-meth <- data %>% select(meth_geo_id, contains('cg'))
+expr <- data %>% 
+  select(expr_geo_id, contains('transcript')) %>%
+  
+  # transpose
+  pivot_longer(cols = -expr_geo_id) %>%
+  pivot_wider(id_cols = name,
+              names_from = expr_geo_id,
+              values_from = value) %>%
+  dplyr::rename(transcript = name)
+
+meth <- data %>% 
+  select(meth_geo_id, contains('cg')) %>%
+  
+  pivot_longer(cols = -meth_geo_id) %>%
+  pivot_wider(id_cols = name,
+              names_from = meth_geo_id,
+              values_from = value) %>%
+  dplyr::rename(cpg = name)
+
+
+
 here::here()
 write_csv(pdat, here::here('workshops', '2020-08-20_reshaping-data', 'pdat.csv'))
 write_csv(expr, here::here('workshops', '2020-08-20_reshaping-data', 'expr.csv'))
